d2de41ab07e047fe962a2990d54534ea9c8078bb
Siavash <siamhb7@protonmail.com>
2023-02-01 22:59:24 +0330

    add toolbar icons and edit toolbar style to be user friendlier.

--- src/shared/toolbarItems.ts
+++ src/shared/toolbarItems.ts
@@ -1,10 +1,30 @@
+import IconEditDirectory from "../assets/icons/iconEditDirectory";
+import IconRemoveDirectory from "../assets/icons/iconRemoveDirectory";
+import IconCopy from "../assets/icons/iconCopy";
+import IconDownload from "../assets/icons/iconDownload";
+import IconEditFile from "../assets/icons/iconEditFile";
+import IconRemoveFile from "../assets/icons/iconRemoveFile";
 import IconStar from "../assets/icons/iconStar";
 import useEx from "../hooks/ex";
 
 const toolbarItems: Omit<ToolBarItemPropsType, "sideInfo">[] = [
   { name: "Star", Icon: IconStar, hook: useEx, type: "file" },
-  { name: "Delete File", Icon: IconStar, hook: useEx, type: "file" },
-  { name: "Delete Folder", Icon: IconStar, hook: useEx, type: "dir" },
+  { name: "Rename File", Icon: IconEditFile, hook: useEx, type: "file" },
+  { name: "Make Copy", Icon: IconCopy, hook: useEx, type: "file" },
+  { name: "Download", Icon: IconDownload, hook: useEx, type: "file" },
+  { name: "Delete File", Icon: IconRemoveFile, hook: useEx, type: "file" },
+  {
+    name: "Delete Dir",
+    Icon: IconRemoveDirectory,
+    hook: useEx,
+    type: "dir",
+  },
+  {
+    name: "Rename Dir",
+    Icon: IconEditDirectory,
+    hook: useEx,
+    type: "dir",
+  },
 ];
 
 export default toolbarItems;
